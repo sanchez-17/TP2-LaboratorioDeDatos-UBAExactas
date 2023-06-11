5e13ee0f6f4fc25360a68e57b5d31ed8b70bdc81
sanchez-17 <gasanchez@dc.uba.ar>
2023-06-11 18:47:54 -0300

    archivo .py

---
+++ digitos_altaData.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#Carga de archivos
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./data/mnist_desarrollo.csv')
+
+#%%
+
+
